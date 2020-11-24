7ffbe0bc172f5b241e1534d9b525efc36fcea153
Song Yi Wei <ywsong@aceinna.com>
2020-11-24 10:51:26 +0800

    fix: add uC uA gC command parser

--- src/aceinna/devices/parsers/open_message_parser.py
+++ src/aceinna/devices/parsers/open_message_parser.py
@@ -10,8 +10,10 @@ from .open_packet_parser import (
 MSG_HEADER = [0x55, 0x55]
 PACKET_TYPE_INDEX = 2
 PRIVATE_PACKET_TYPE = ['RE', 'WE', 'UE', 'LE', 'SR']
-INPUT_PACKETS = ['pG', 'gV', 'gA', 'gB', 'gP', 'sC', 'uP', 'uB',
-                 'rD', '\x15\x15', '\x00\x00', 'ma',
+INPUT_PACKETS = ['pG', 'uC', 'uP',  'uA', 'uB'
+                 'sC', 'rD',
+                 'gC', 'gA',  'gB', 'gP', 'gV',
+                 '\x15\x15', '\x00\x00', 'ma',
                  'JI', 'JA', 'WA',
                  'RE', 'WE', 'UE', 'LE', 'SR']
 OTHER_OUTPUT_PACKETS = ['CD', 'CB']
